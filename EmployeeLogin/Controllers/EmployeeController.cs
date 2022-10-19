93c3937f915eca5429c70202acefdb9cfac61745
STS666-RAVISESHU T <STS666-RAVISESHU T@STS666L-RAVISHE>
2022-10-19 11:05:29 +0530

    Add project files.

---
+++ EmployeeLogin/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using EmployeeLogin.Models;
+namespace WebSite.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Pavan
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        public ActionResult Login(Model v)
+        {
+            if (v.UserName == "seshu" && v.Password == "123")
+            {
+                ViewBag.s = "Hi";
+                ViewData["result"] = "Welcome";
+                Model employee = new Model()
+                {
+                    EmployeeId = 101,
+                    EmployeeName = "Ravi seshu",
+                    EmployeeAddress = "kakinada",
+                    EmployeePhoneNo = 8341358889,
+                    IsPermenent = false,
+                };
+                return View("Info", employee);
+            }
+            else
+            {
+                ViewBag.x = "Invalid Username or Password";
+                return View("Login");
+            }
+        }
+
+    }
+}
---
+++ EmployeeLogin/Models/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeLogin.Models
+{
+    public class Model
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public long EmployeePhoneNo { get; set; }
+        public bool IsPermenent { get; set; }
+    }
+}
